1df0800cde5f2e299a9eb2fd03722aafdcff44a3
Robert Kelley <robertkelley1987@gmail.com>
2024-05-17 13:23:42 -0400

    Trying different cors url as origin.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://3.144.152.142",
+    origin: "http://ec2-3-144-152-142.us-east-2.compute.amazonaws.com",
     credentials: true,
   })
 );
